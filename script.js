f7f08bf5ad41dc30d5007c891d37e540eb1e5d54
Konrad <w.konrad60@gmail.com>
2017-09-29 23:41:23 +0200

    zadanie 9.5

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-function getTriangleArea (a, h) {
-
-	if((a > 0)&&(h > 0)) {
+window.onload = function() {
+	var button = document.getElementsByClassName("button");
+	for(var i = 0; i < button.length; i++) {
+		console.log(button[i].innerText);
 	}
-	else {
-		console.log("Nieprawidłowe dane")
-	}
-	return a*h/2;
 }
-
-//main
-
-console.log("To jest pierwszy wynik: " + getTriangleArea(6,10));
-
-var triangle1Area = getTriangleArea(10,15);
-var triangle2Area = getTriangleArea(11,16);
-var triangle3Area = getTriangleArea(12,17);
-
-console.log("Pierwsza zmienna to: " + triangle1Area);
-console.log("Druga zmienna to: " + triangle2Area);
-console.log("Trzecia zmienna to: " + triangle3Area);
