b074c97a98ae38be16236730bbe310948d601231
Konrad <w.konrad60@gmail.com>
2017-09-29 19:22:13 +0200

    zadanie9.4

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-function getTriangleArea (a, h) {
-
-	if((a > 0)&&(h > 0)) {
-	}
-	else {
-		console.log("Nieprawidłowe dane")
-	}
-	return a*h/2;
+var drawTree = function(treeHeight) {
+    var star = "";
+    for (var i = 0; i < treeHeight; i++) {
+        star += "*";
+        console.log(star);
+    }
 }
 
 //main
 
-console.log("To jest pierwszy wynik: " + getTriangleArea(6,10));
-
-var triangle1Area = getTriangleArea(10,15);
-var triangle2Area = getTriangleArea(11,16);
-var triangle3Area = getTriangleArea(12,17);
-
-console.log("Pierwsza zmienna to: " + triangle1Area);
-console.log("Druga zmienna to: " + triangle2Area);
-console.log("Trzecia zmienna to: " + triangle3Area);
+var HeightTree = prompt('Podaj wysokosc choinki :)');
+drawTree(HeightTree);
